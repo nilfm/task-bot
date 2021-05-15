2137a2109eb8a9af348b3c9d3a6f2043e80302b8
nilfm <nil.fons@gmail.com>
2021-05-15 13:15:33 +0200

    Add shopgroups and id

--- bot.py
+++ bot.py
@@ -9,21 +9,28 @@ def start(update, context):
         text=f"Hi {name}!"
     )
 
+def get_id(update, context):
+    user_id = update.message.chat.id
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text=f"{user_id}"
+    )
+
 def parse_add_shopping_list(args):
-    if len(args) == 1:
-        raise ValueError()
+    if len(args) == 0:
+        raise ValueError("Invalid command: nothing to buy.")
     items = defaultdict(int)
     forced_word = False
     prev_num = 1
     if args[-1].isdigit():
-        raise ValueError()
-    for word in args[1:]:
+        raise ValueError("Invalid command: ends with a number.")
+    for word in args:
         if word.isdigit():
             if forced_word:
-                raise ValueError()
+                raise ValueError("Invalid command: two consecutive numbers.")
             num = int(word)
             if num == 0:
-                raise ValueError()
+                raise ValueError("Invalid command: can't buy 0 things.")
             prev_num = num
             forced_word = True
         else:
@@ -33,12 +40,12 @@ def parse_add_shopping_list(args):
     return items
 
 def parse_remove_shopping_list(args):
-    if len(args) == 1:
-        raise ValueError()
+    if len(args) == 0:
+        raise ValueError("Invalid command: nothing to remove.")
     items = set()
-    for word in args[1:]:
+    for word in args:
         if word.isdigit():
-            raise ValueError()
+            raise ValueError(f"Invalid command: {word} is a number.")
         else:
             items.add(word)
     return items
@@ -51,18 +58,53 @@ def shop(update, context):
         if len(args) == 0 or (len(args) == 1 and args[0] == "list"):
             message = shopping.show_list(user_id)
         elif args[0] == "add":
-            to_buy = parse_add_shopping_list(args)
+            to_buy = parse_add_shopping_list(args[1:])
             message = shopping.add(user_id, to_buy)
         elif args[0] == "remove":
-            to_remove = parse_remove_shopping_list(args)
+            to_remove = parse_remove_shopping_list(args[1:])
             message = shopping.remove(user_id, to_remove)
         elif len(args) == 1 and args[0] == "clear":
             message = shopping.clear(user_id)
         else:
-            raise ValueError()
+            raise ValueError("I couldn't understand you :(")
         context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-    except ValueError:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="I couldn't understand you :(")
+    except ValueError as e:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=str(e))
+
+def shopgroup(update, context):
+    user_id = update.message.chat.id
+    args = context.args
+
+    try:
+        if len(args) == 0:
+            raise ValueError("Invalid command: no shopgroup specified.")
+        elif len(args) == 1 or (len(args) == 2 and args[1] == "list"):
+            message = shopping.show_list(user_id, args[0])
+        elif args[0] == "create":
+            if len(args) != 3:
+                raise ValueError("Invalid command: try /shopgroup create <groupname> <password>")
+            message = shopping.create_shopgroup(user_id, args[1], args[2])
+        elif args[0] == "join":
+            if len(args) != 3:
+                raise ValueError("Invalid command: try /shopgroup join <groupname> <password>")
+            message = shopping.join_shopgroup(user_id, args[1], args[2])
+        elif args[0] == "leave":
+            if len(args) != 2:
+                raise ValueError("Invalid command: try /shopgroup leave <groupname>")
+            message = shopping.leave_shopgroup(user_id, args[1])
+        elif args[1] == "add":
+            to_buy = parse_add_shopping_list(args[2:])
+            message = shopping.add(user_id, to_buy, args[0])
+        elif args[1] == "remove":
+            to_remove = parse_remove_shopping_list(args[2:])
+            message = shopping.remove(user_id, to_remove, args[0])
+        elif args[1] == "clear":
+            message = shopping.clear(user_id, args[0])
+        else:
+            raise ValueError("I couldn't understand you :(")
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    except ValueError as e:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=str(e))
 
 
 # TODO: Edit distance for the remove/edit options 
@@ -74,6 +116,8 @@ updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('id', get_id))
 dispatcher.add_handler(CommandHandler('shop', shop, pass_args=True))
+dispatcher.add_handler(CommandHandler('shopgroup', shopgroup, pass_args=True))
 
 updater.start_polling()
--- shopping.py
+++ shopping.py
@@ -1,9 +1,15 @@
 import json
 import utils
+import os
 
-def show_list(user_id):
+def show_list(user_id, group_id=None):
     utils.create_user(user_id)
     path = f"data/{user_id}/shopping.json"
+
+    if group_id is not None:
+        utils.verify_shopgroup(user_id, group_id)
+        path = f"data/shopgroups/{group_id}.json"
+
     with open(path, "r") as f:
         current = json.load(f)
     if len(current) == 0:
@@ -14,31 +20,68 @@ def show_list(user_id):
     ]
     return "\n".join(lines)
 
-def add(user_id, to_buy):
+def add(user_id, to_buy, group_id=None):
     utils.create_user(user_id)
     path = f"data/{user_id}/shopping.json"
+
+    if group_id is not None:
+        utils.verify_shopgroup(user_id, group_id)
+        path = f"data/shopgroups/{group_id}.json"
+
     with open(path, "r") as f:
         current = json.load(f)
     for name, amt in to_buy.items():
         current[name] = current.get(name, 0) + amt
     with open(path, "w") as f:
         json.dump(current, f, indent=4)
-    return show_list(user_id)
+    return show_list(user_id, group_id)
 
-def remove(user_id, to_remove):
+def remove(user_id, to_remove, group_id=None):
     utils.create_user(user_id)
     path = f"data/{user_id}/shopping.json"
+
+    if group_id is not None:
+        utils.verify_shopgroup(user_id, group_id)
+        path = f"data/shopgroups/{group_id}.json"
+
     with open(path, "r") as f:
         current = json.load(f)
     for name in to_remove:
         current.pop(name, None)
     with open(path, "w") as f:
         json.dump(current, f, indent=4)
-    return show_list(user_id) 
+    return show_list(user_id, group_id) 
 
-def clear(user_id):
+def clear(user_id, group_id=None):
     utils.create_user(user_id)
     path = f"data/{user_id}/shopping.json"
+
+    if group_id is not None:
+        utils.verify_shopgroup(user_id, group_id)
+        path = f"data/shopgroups/{group_id}.json"
+
     with open(path, "w") as f:
         f.write("{}")
-    return show_list(user_id)
+    return show_list(user_id, group_id)
+
+def create_shopgroup(user_id, group_id, password):
+    utils.create_user(user_id)
+    path_dir = "data/shopgroups"
+    path = f"{path_dir}/{group_id}.json"
+    if os.path.isfile(path):
+        raise ValueError("Invalid command: a shopgroup with this name already exists.")
+    os.makedirs(path_dir, exist_ok=True)
+    utils.create_json_dict(path)
+    utils.update_shopgroup_list(group_id, password)
+    utils.add_user_to_shopgroup(user_id, group_id)
+    return f"Shopgroup {group_id} was successfully created!"
+
+def join_shopgroup(user_id, group_id, password):
+    utils.create_user(user_id)
+    utils.authenticate_shopgroup(group_id, password)
+    utils.add_user_to_shopgroup(user_id, group_id)
+    return f"Joined shopgroup {group_id} successfully!"
+
+def leave_shopgroup(user_id, group_id):
+    utils.remove_user_from_shopgroup(user_id, group_id)
+    return f"Left shopgroup {group_id} successfully!"
